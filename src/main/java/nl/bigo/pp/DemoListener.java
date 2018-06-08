f3c6f7d8829d427df618d4fa9725d598696ae034
Dennis Piskovatskov <dennis.piskovatskov@javaee.solutions>
2018-06-08 16:35:57 +0200

    chess.com pgn and pgn have different formats (listener will show it)

--- src/main/java/nl/bigo/pp/DemoListener.java
+++ src/main/java/nl/bigo/pp/DemoListener.java
@@ -27,52 +27,69 @@
  * Project      : A Portable Game Notation (PGN) ANTLR 4 grammar
  *                and parser.
  * Developed by : Bart Kiers, bart@big-o.nl
+ *                Dennis Piskovatskov, dennis.piskovatskov@javaee.solutions
  */
 package nl.bigo.pp;
 
 import org.antlr.v4.runtime.misc.NotNull;
 
 /**
- * A small demo listener that retrieves the player's name and
- * the result from the game termination production rule. Of course
- * the result could more easily be retrieved from the result-tag,
- * [Result "..."], but this is just a quick example of how to
- * retrieve information from the parser context objects.
+ * A small demo listener that retrieves the player's name and the result from
+ * the game termination production rule. Of course the result could more easily
+ * be retrieved from the result-tag, [Result "..."], but this is just a quick
+ * example of how to retrieve information from the parser context objects.
  */
 public class DemoListener extends PGNBaseListener {
 
-  private String white = null;
-  private String black = null;
+	private String white;
+	private String black;
 
-  // Grammar production rule:
-  //
-  //   tag_pair
-  //    : LEFT_BRACKET tag_name tag_value RIGHT_BRACKET
-  //    ;
-  @Override
-  public void enterTag_pair(@NotNull PGNParser.Tag_pairContext ctx) {
+	// Grammar production rule:
+	//
+	// tag_pair
+	// : LEFT_BRACKET tag_name tag_value RIGHT_BRACKET
+	// ;
+	@Override
+	public void enterTag_pair(@NotNull PGNParser.Tag_pairContext ctx) {
+		final String tagName = ctx.tag_name().getText();
 
-    String tagName = ctx.tag_name().getText();
+		if (tagName.equals("White")) {
+			white = ctx.tag_value().getText().replace("\"", "");
+		} else if (tagName.equals("Black")) {
+			black = ctx.tag_value().getText().replace("\"", "");
+		}
+	}
 
-    if(tagName.equals("White")) {
-      white = ctx.tag_value().getText().replace("\"", "");
-    }
-    else if(tagName.equals("Black")) {
-      black = ctx.tag_value().getText().replace("\"", "");
-    }
-  }
+	// Grammar production rule:
+	//
+	// movetext_section
+	// : element_sequence game_termination
+	// ;
+	@Override
+	public void enterMovetext_section(@NotNull PGNParser.Movetext_sectionContext ctx) {
+		final String result = ctx.game_termination().getText();
+		System.out.printf("`%s` with white against `%s` with black resulted in: %s\n", white, black, result);
+	}
 
-  // Grammar production rule:
-  //
-  //   movetext_section
-  //    : element_sequence game_termination
-  //    ;
-  @Override
-  public void enterMovetext_section(@NotNull PGNParser.Movetext_sectionContext ctx) {
+	// Grammar production rule:
+	// element
+	// : move_number_indication
+	// | san_move
+	// | NUMERIC_ANNOTATION_GLYPH
+	// ;
+	@Override
+	public void enterElement_sequence(@NotNull PGNParser.Element_sequenceContext ctx) {
+		System.out.println("element: " + ctx.getText());
+	}
 
-    String result = ctx.game_termination().getText();
+	// Grammar production rule:
+	//
+	// san_move
+	// : SYMBOL
+	// ;
+	@Override
+	public void enterSan_move(@NotNull PGNParser.San_moveContext ctx) {
+		System.out.println("san_move: " + ctx.getText());
+	}
 
-    System.out.printf("`%s` with white against `%s` with black resulted in: %s\n",
-        white, black, result);
-  }
 }
